cdcd7b2cd9e1a877a48fb9a8d0953b4adce9e1a1
virus53111 <151654868+virus53111@users.noreply.github.com>
2026-09-19 18:29:29 +0300

    Load clean stream mode helper

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import './live-onboarding';
 import './phone-mode';
+import './stream-mode';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
